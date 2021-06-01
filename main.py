65a6c22e1d38f34352dd671df6ff3b08b26988f6
Manish Yadav <manishyadav8286@gmail.com>
2021-06-01 16:09:21 +0530

    modified progressbar

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import time
 from tkinter import HORIZONTAL
 
 
-
 root = tkinter.Tk()
 #window.geometry("1080x720")
 root.state('zoomed')
@@ -23,11 +22,11 @@ root.iconbitmap("")
 root.title("MyStock")
 
 # pack is used to show the object in the window
-label = tkinter.Label(root, text = "Stock Analysis App for Simple Moving Average").pack()
+#label = tkinter.Label(root, text = "Stock Analysis App for Simple Moving Average").pack()
 
 # creating functions 
 
-def Scanner():
+def scanner():
     nifty=pd.read_csv('NIFTY200.csv')
     #time.sleep(60)
     # Get historical data of all Nifty50 stocks and filter out rising stocks
@@ -35,14 +34,16 @@ def Scanner():
     # and add the result as a new column to dataframe
     list=[]
     updated=True
+    
     for name, values in nifty.iteritems():
         for index in values:
             rising_stock=get_history(index,start=datetime(2021,1,1),end=datetime(datetime.now().year,datetime.now().month,datetime.now().day))
             sublist=[]
+            root.update()
             if rising_stock.shape[0]>1 and rising_stock['Open'][-1] < rising_stock['Close'][-1]:
                 short_window = 44
                 rising_stock["44_obs_for_SMA"] = rising_stock["Close"].rolling(window=short_window).mean()
-                
+                root.update()
                 if (rising_stock["High"][-1]>= rising_stock["44_obs_for_SMA"][-1] and rising_stock["Low"][-1]<= rising_stock["44_obs_for_SMA"][-1]):
                     sublist.append(index)
                     sublist.append(rising_stock["Open"][-1])
@@ -56,31 +57,40 @@ def Scanner():
 
                     rising_stock.to_csv("stocks/{index}.csv".format(index=index))
                     print(index)
+                    root.update()
                 
         #print('{name}: {value}'.format(name=name, value=values))
     data=pd.DataFrame(list,columns=["SYMBOL","Open","High","Low","Close","44_obs_for_SMA"])
     data=data[["SYMBOL","Open","High","Low","Close","44_obs_for_SMA"]]
     data.to_csv("stocks/rising_stocks.csv")
-    #plot_chart()
+    plot_chart()
+    
 
 def plot_chart():
     mc = mpf.make_marketcolors(up='g',down='r',edge='black',volume='gray',ohlc='black')
     s  = mpf.make_mpf_style(marketcolors=mc)
     rising_stocks=pd.read_csv('stocks/rising_stocks.csv',index_col=0)
     for index, row in rising_stocks.iterrows():
+        root.update()
         stock = pd.read_csv('stocks/{symbol}.csv'.format(symbol=row["SYMBOL"]),index_col=0,parse_dates=True)
-        #ourpath = pathlib.Path("img/{symbol}.png".format(symbol=row["SYMBOL"]))
-        #mpf.plot(stock, type='candle',figratio=(100,40), mav=44, savefig=ourpath,style=s)
-        mpf.plot(stock,type='candle',figratio=(38,15),mav=44,style=s)
+        ourpath = pathlib.Path("img/{symbol}.png".format(symbol=row["SYMBOL"]))
+        mpf.plot(stock, type='candle',figratio=(100,40), mav=44, savefig=ourpath,style=s)
+        #mpf.plot(stock,type='candle',figratio=(38,15),mav=44,style=s)
 
 
 
 def step():
-    my_progress['value'] += 20
+    #my_progress['value'] += 20
+    my_progress= Progressbar(root, orient=HORIZONTAL, length=300, mode="indeterminate")
+    my_progress.pack(pady=20)
+    my_progress.start(1)
+    scanner()
+    my_progress.stop()
+    my_progress.destroy()
 
-my_progress= Progressbar(root, orient=HORIZONTAL, length=300, mode="indeterminate")
-my_progress.pack(pady=20)
-#tkinter.Button(root, text = "Click Me!", command = Scanner).pack(pady=20)
+
+
+#tkinter.Button(root, text = "Click Me!", command = scanner).pack(pady=20)
 my_button=tkinter.Button(root, text="Progress", command = step)
 my_button.pack(pady=20)
 
