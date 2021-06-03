a8d7f0d5124243ff8abe5034c0bf31026b71addf
Manish Yadav <manishyadav8286@gmail.com>
2021-06-03 16:33:33 +0530

    added candelstick chart on gui

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from itertools import cycle
 from PIL import ImageTk, Image
+import glob
+import os
 
 root = tkinter.Tk()
 #window.geometry("1080x720")
@@ -99,124 +101,111 @@ def slideShow():
   #root.after(50, slideShow) # 0.05 seconds
 
 
-def left_layout():
-    # left frame
-    left_frame=Frame(frame,bg="blue")
-    
-    # list1 frame inside left frame
-    list_frame= Frame(left_frame)
-    list_scrollbar= Scrollbar(list_frame, orient=VERTICAL)
-    list1=Listbox(list_frame, width=30,height=15, yscrollcommand=list_scrollbar.set)
-    label1=Label(list_frame, text="NSE Indices", width=30,  bg="#2962ff" ,fg="#FFFFFF")
-    label1.pack()
-
-    list_scrollbar.config(command=list1.yview)
-    list_scrollbar.pack(side=RIGHT,fill=Y)
-    list_frame.pack(fill=X)
-
-    list1.pack()
-    list1.insert(END,"Nifty50")
-    list1.insert(END,"Nifty100")
-    list1.insert(END,"Nifty200")
-
-    # list2 frame inside left frame
-    list_frame2= Frame(left_frame)
-    list_scrollbar= Scrollbar(list_frame2, orient=VERTICAL)
-    list2=Listbox(list_frame2, width=30,height=15, yscrollcommand=list_scrollbar.set)
-    label2=Label(list_frame2, text="Rising Stocks", width=30,  bg="#2962ff" ,fg="#FFFFFF")
-    label2.pack()
-
-    list_scrollbar.config(command=list2.yview)
-    list_scrollbar.pack(side=RIGHT,fill=Y)
-    list_frame2.pack(fill=X)
-
-    list2.pack()
-    list2.insert(END,"Nifty50")
-    list2.insert(END,"Nifty100")
-    list2.insert(END,"Nifty200")
-
-    left_frame.pack(side=LEFT, fill=Y)
-
-def header_layout():
-    # header frame for menu
-    header_frame=Frame(frame,bg="yellow")
-    button1=Button(header_frame, text="Home", width=15, bg="#2962ff" ,fg="#FFFFFF", command = step)
-    button1.grid(row=0,column=0,sticky="nsew")
-    button2=Button(header_frame, text="Live Market", width=15, bg="#2962ff" ,fg="#FFFFFF", command = plot)
-    button2.grid(row=0,column=1,sticky="nsew")
-    button3=Button(header_frame, text="Scan MA stocks", width=15, bg="#2962ff" ,fg="#FFFFFF", command = step)
-    button3.grid(row=0,column=3,sticky="nsew")
-    header_frame.pack(fill=X)
-
-def middle_layout():
-    # middle frame layout
-    chart_frame=Frame(frame,bg="orange")
-    lb=Label(chart_frame,text="test",width=30)
-    lb.pack()
-    chart_frame.pack(side=LEFT, fill=BOTH, expand=True)
+def insertfiles():
+    # lst.delete(0, tk.END)
+    for filepath in glob.glob("img/*.png"):
+        filename=os.path.basename(filepath).split('.', 1)[0]
+        list2.insert(END, filename)
+def delete_item(event):
+    n = lst.curselection()
+    os.remove(lst.get(n))
+    lst.delete(n)
 
 
-def right_layout():
-    # right frame layout
-    right_frame=Frame(frame,bg="yellow")
-    lb=Label(frame,text="test2",width=30)
-    lb.pack()
-    right_frame.pack(side=RIGHT, fill=Y)
-# left frame layout
-# left_frame=Frame(frame,bg="blue")
-# lb=Label(left_frame,text="test",width=30)
-# lb.pack()
-# left_frame.pack(side=LEFT, fill=Y)
+def get_window_size():
+    if chart_frame.winfo_width() > 200 and root.winfo_height() >30:
+        w = chart_frame.winfo_width() - 200
+        h = chart_frame.winfo_height() - 30
+    else:
+        w = 200
+        h = 30
+    return w, h
+
+
+def showing(event):
+    n = list2.curselection()
+    filename = list2.get(n)
+    im = Image.open("img/{filename}.png".format(filename=filename))
+    im = im.resize((get_window_size()), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(im)
+    w, h = img.width(), img.height()
+    canvas.image = img
+    canvas.config(width=w, height=h)
+    canvas.create_image(0, 0, image=img, anchor=NW)
+    chart_frame.bind("<Configure>", lambda x: showing(x))
+
 
 # parent frame
 frame=Frame(root,bg="red")
-frame.pack(fill="both",expand=True)
-
-header_layout()
-left_layout()
-middle_layout()
-right_layout()
+frame.pack(fill="both",expand=True)   
 
 
+# header frame for menu
+header_frame=Frame(frame,bg="yellow")
+button1=Button(header_frame, text="Home", width=15, bg="#2962ff" ,fg="#FFFFFF", command = step)
+button1.grid(row=0,column=0,sticky="nsew")
+button2=Button(header_frame, text="Live Market", width=15, bg="#2962ff" ,fg="#FFFFFF", command = plot)
+button2.grid(row=0,column=1,sticky="nsew")
+button3=Button(header_frame, text="Scan MA stocks", width=15, bg="#2962ff" ,fg="#FFFFFF", command = step)
+button3.grid(row=0,column=3,sticky="nsew")
+header_frame.pack(fill=X)
+
+# left frame
+left_frame=Frame(frame,bg="blue")
+
+# list1 frame inside left frame
+list_frame= Frame(left_frame)
+list_scrollbar= Scrollbar(list_frame, orient=VERTICAL)
+list1=Listbox(list_frame, width=30,height=15, yscrollcommand=list_scrollbar.set)
+label1=Label(list_frame, text="NSE Indices", width=30,  bg="#2962ff" ,fg="#FFFFFF")
+label1.pack()
+
+list_scrollbar.config(command=list1.yview)
+list_scrollbar.pack(side=RIGHT,fill=Y)
+list_frame.pack(fill=X)
+
+list1.pack(fill=X, expand=True)
+list1.insert(END,"Nifty50")
+list1.insert(END,"Nifty100")
+list1.insert(END,"Nifty200")
+
+# list2 frame inside left frame
+list_frame2= Frame(left_frame)
+list_scrollbar= Scrollbar(list_frame2, orient=VERTICAL)
+list2=Listbox(list_frame2, width=30,height=25, yscrollcommand=list_scrollbar.set)
+label2=Label(list_frame2, text="Rising Stocks", width=30,  bg="#2962ff" ,fg="#FFFFFF")
+label2.pack()
+
+list_scrollbar.config(command=list2.yview)
+list_scrollbar.pack(side=RIGHT,fill=Y)
+list_frame2.pack(fill=X)
+
+list2.pack(fill=X, expand=True)
+list2.bind("<<ListboxSelect>>", showing)
+insertfiles()
+
+
+left_frame.pack(side=LEFT, fill=Y)
+
+
+
+# middle frame layout
+chart_frame=Frame(frame,bg="orange")
+lb=Label(chart_frame,text="test",width=30)
+lb.pack()
+canvas = Canvas(chart_frame)
+canvas.pack()
+chart_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+
+
+# right frame layout
+right_frame=Frame(frame,bg="yellow")
+lb=Label(frame,text="test2",width=30)
+lb.pack()
+right_frame.pack(side=RIGHT, fill=Y)
 
 
 
 
-
-
-
-
-
-
-# list_frame2= Frame(root)
-# list_scrollbar2= Scrollbar(list_frame2, orient=VERTICAL)
-# rising_stock_label=Label(list_frame2, text="Rising Stocks", width=25, bg="#2962ff" ,fg="#FFFFFF")
-# rising_stock_list=Listbox(list_frame2, width=25)
-# rising_stock_list.insert(END,"CUB")
-# rising_stock_list.bind("<<ListboxSelect>>", plot)
-# list_scrollbar2.config(command=my_list.yview)
-
-# list_scrollbar2.pack(side=RIGHT,fill=Y)
-# rising_stock_label.pack()
-# rising_stock_list.pack()
-# list_frame2.grid(row=3, column=0)
-
-
-# # images = ["TCS.png", "IGL.png"]
-# # photos = cycle(ImageTk.PhotoImage(Image.open("img/{symbol}".format(symbol=image))) for image in images)
-# image = Image.open("img/TCS.png")
-  
-# # Reszie the image using resize() method
-# resize_image = image.resize((1100,650))
-  
-# img = ImageTk.PhotoImage(resize_image)
-
-# main_frame=Frame(root, bg="blue",width=100,height=100)
-# width = main_frame.winfo_screenwidth()
-# height = main_frame.winfo_screenwidth()
-# main_label=Label(main_frame)
-# main_label.pack()
-# main_frame.grid(row=2,column=1, columnspan=99, rowspan=20)
-
-#root.after(10, lambda: slideShow())
 root.mainloop()
